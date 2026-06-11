f0c9a245f1deee6ec1ae3bffcd17609ccfc670c2
yooan321 <yooanean.99@gmail.com>
2026-06-11 23:29:03 +0100

    import packages

--- 1. Linear Regression.py
+++ 1. Linear Regression.py
@@ -1,1 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+import time
+# Generate synthetic data
+np.random.seed(0)
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
